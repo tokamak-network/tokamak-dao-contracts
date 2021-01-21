7062cc2f0c076a1363917f43856cdb6fd9f63ab4
modagi <modagi@gmail.com>
2021-01-21 07:31:13 +0000

    contracts: layer2 contract can be a candidate

--- contracts/dao/Candidate.sol
+++ contracts/dao/Candidate.sol
@@ -1,45 +1,106 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-import "../shared/Ownabled.sol";
+import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 import { IDAOCommittee } from "../interfaces/IDAOCommittee.sol";
 import { IERC20 } from  "../../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeMath } from "../../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 import { ISeigManager } from "../interfaces/ISeigManager.sol";
 import { ICandidate } from "../interfaces/ICandidate.sol";
+import { ILayer2 } from "../interfaces/ILayer2.sol";
 import { ILayer2Registry } from "../interfaces/ILayer2Registry.sol";
+import { IDAOCommittee } from "../interfaces/IDAOCommittee.sol";
 import { ERC165 } from "../../node_modules/@openzeppelin/contracts/introspection/ERC165.sol";
 
-contract Candidate is Ownabled, ICandidate, ERC165 {
+contract Candidate is Ownable, ICandidate, ERC165 {
     using SafeMath for uint256;
     address public candidate;
-    address public seigManager;
+    ISeigManager public seigManager;
     string public memo;
+    ILayer2 public layer2;
+    IDAOCommittee public committee;
+
+    modifier onlyCandidate() {
+        if (address(layer2) != address(0)) {
+            require(layer2.isLayer2(), "Candidate: invalid layer2 contract");
+            require(layer2.operator() == msg.sender, "Candidate: invalid candidate");
+        } else {
+            require(msg.sender == candidate, "Candidate: sender is not ");
+        }
+        _;
+    }
     
-    constructor(address _candidate, string memory _memo) {
+    constructor(
+        address _candidate,
+        address _layer2,
+        address _committee,
+        address _seigManager,
+        string memory _memo
+    ) 
+    {
+        if (_layer2 != address(0)) {
+            layer2 = ILayer2(_layer2);
+            require(layer2.isLayer2(), "Candidate: invalid layer2 contract");
+            require(layer2.operator() == _candidate, "Candidate: invalid candidate");
+        }
+
+        committee = IDAOCommittee(_committee);
+        seigManager = ISeigManager(_seigManager);
         candidate = _candidate;
         memo = _memo;
 
         _registerInterface(ICandidate(address(this)).isCandidateContract.selector);
     }
     
-    function setSeigManager(address _seigMan) public onlyOwner override {
-        seigManager =_seigMan;
+    function setSeigManager(address _seigManager) public onlyOwner override {
+        seigManager = ISeigManager(_seigManager);
+    }
+
+    function setCommittee(address _committee) public onlyOwner override {
+        committee = IDAOCommittee(_committee);
     }
 
     function updateSeigniorage() public override returns (bool) {
-        require(seigManager != address(0), "Candidate: SeigManager is zero");
+        require(address(seigManager) != address(0), "Candidate: SeigManager is zero");
+        require(
+            address(layer2) == address(0),
+            "Candidate: you should update seigniorage from layer2 contract"
+        );
+
         return ISeigManager(seigManager).updateSeigniorage();
     }
-    
-    function isCommitteeLayer2() public override view returns (bool) {
-        return true;
+
+    function changeMember(uint256 _memberIndex)
+        public
+        onlyCandidate
+        returns (bool)
+    {
+        return committee.changeMember(_memberIndex, msg.sender);
     }
 
-    function candidateAndOwner() public override view returns (address, address) {
-        return (candidate, owner);
+    function retireMember() public onlyCandidate returns (bool) {
+        return committee.retireMember();
     }
+    
+    function castVote(
+        uint256 _agendaID,
+        uint _vote,
+        string calldata _comment
+    )
+        public
+        onlyCandidate
+    {
+        committee.castVote(_agendaID, _vote, _comment);
+    }
+
+    /*function isCommitteeLayer2() public override view returns (bool) {
+        return true;
+    }*/
+
+    /*function candidateAndOwner() public override view returns (address, address) {
+        return (candidate, owner);
+    }*/
 
     function isCandidateContract() public override view returns (bool) {
         return true;
@@ -49,7 +110,51 @@ contract Candidate is Ownabled, ICandidate, ERC165 {
     function isLayer2() public view returns (bool) { return true; }
     function currentFork() public view returns (uint) { return 1; }
     function lastEpoch(uint forkNumber) public view returns (uint) { return 1; }
-    /*function changeCandidate(address _candidate) public {
-        candidate = _candidate;
+
+    /*function isCandidate(address _account) public view {
     }*/
+
+    function totalStaked()
+        public
+        view
+        override
+        returns (uint256 totalsupply)
+    {
+        address coinage = _getCoinageToken();
+        require(coinage != address(0), "Candidate: coinage is zero");
+        return IERC20(coinage).totalSupply();
+    }
+
+    function stakedOf(
+        address _account
+    )
+        public
+        view
+        override
+        returns (uint256 amount)
+    {
+        address coinage = _getCoinageToken();
+        require(coinage != address(0), "DAOCommittee: coinage is zero");
+        return IERC20(coinage).balanceOf(_account);
+    }
+
+    function getCandidate() public view override returns (address) {
+        if (address(layer2) != address(0)) {
+            return layer2.operator();
+        } else {
+            return candidate;
+        }
+    }
+
+    function _getCoinageToken() internal view returns (address) {
+        address c;
+        if (address(layer2) != address(0)) {
+            c = address(layer2);
+        } else {
+            c = address(this);
+        }
+
+        return seigManager.coinages(c);
+
+    }
 }
--- contracts/factory/CandidateFactory.sol
+++ contracts/factory/CandidateFactory.sol
@@ -5,12 +5,25 @@ import { Candidate } from "../dao/Candidate.sol";
 import { ICandidateFactory } from "../interfaces/ICandidateFactory.sol";
 
 contract CandidateFactory is ICandidateFactory {
-    // layer2 deploy... make coinages
-    function deploy(address _candidate, string memory _name, address _seigManager, address _layer2registry) public override returns (address) {
-        Candidate c = new Candidate(_candidate, _name);
-        c.setSeigManager(_seigManager);
-        //c.setLayer2Registry(_layer2registry);
-        c.transferOwnership(_candidate);
+    function deploy(
+        address _candidate,
+        address _layer2,
+        string memory _name,
+        address _seigManager,
+        address _committee
+    )
+        public
+        override
+        returns (address)
+    {
+        Candidate c = new Candidate(
+            _candidate,
+            _layer2,
+            _committee,
+            _seigManager,
+            _name
+        );
+        c.transferOwnership(_committee);
         return address(c);
     }
 }
