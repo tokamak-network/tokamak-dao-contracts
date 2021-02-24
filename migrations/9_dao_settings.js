4ba476b535355d737c5d418e74456870507ef364
modagi <modagi@gmail.com>
2021-02-24 04:35:09 +0000

    migration: updated

--- migrations/9_dao_settings.js
+++ migrations/9_dao_settings.js
@@ -1,7 +1,7 @@
 const save = require('../utils/save_deployed');
 const load = require('../utils/load_deployed');
 
-const DAOVault2 = artifacts.require('DAOVault2');
+const DAOVault = artifacts.require('DAOVault');
 const DAOAgendaManager = artifacts.require('DAOAgendaManager');
 const CandidateFactory = artifacts.require('CandidateFactory');
 const DAOCommittee = artifacts.require('DAOCommittee');
@@ -10,11 +10,11 @@ const DAOCommitteeProxy = artifacts.require('DAOCommitteeProxy');
 module.exports = async function (deployer, network) {
   if (process.env.SET_DAO) {
     const committeeProxyAddress = load(network, "DAOCommitteeProxy");
-    const daoVault2Address = load(network, "DAOVault2");
+    const daoVault2Address = load(network, "DAOVault");
     const agendaManagerAddress = load(network, "DAOAgendaManager");
     const committeeAddress = load(network, "DAOCommittee");
 
-    const daoVault2 = await DAOVault2.at(daoVault2Address);
+    const daoVault2 = await DAOVault.at(daoVault2Address);
     const agendaManager = await DAOAgendaManager.at(agendaManagerAddress);
     const committeeProxy = await DAOCommittee.at(committeeProxyAddress);
     const committee = await DAOCommittee.at(committeeAddress);
@@ -23,6 +23,5 @@ module.exports = async function (deployer, network) {
     await daoVault2.transferOwnership(committeeProxy.address);
     await agendaManager.setCommittee(committeeProxy.address);
     await agendaManager.transferOwnership(committeeProxy.address);
-    //await committee.transferOwnership(committeeProxy.address);
   }
 };
